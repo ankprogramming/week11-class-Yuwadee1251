e41625345f1db6bfc62451ff2647008af197947a
Unknown <yuwadee.dd58@gmail.com>
2018-02-06 11:54:53 +0700

    แก้ใหม่

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     string str = "There are two needles in this haystack with needles.";
     cout << str << endl;
 
@@ -11,9 +12,24 @@ int main() {
 
     str += "I Want to eat Needle.";
     cout << str;
+            << endl;
     cout << "Length : " << str.length() << endl;
 
     str.erase(str.length()/2, str.length()/2);
+    cout << str;
+            << endl;
+    cout << "Length : " << str.length() << endl;
+
+    if (str.find("needle") < str.length())
+    {
+        cout << "Found 'needle'"; << endl;
+    }
+    else3
+    {
+        cout << "Not Found 'needle'" << endl;
+
+    }
+
 
     return 0;
 }
